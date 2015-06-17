235cd03c0e06d86a6ecb44a118c95eb3a98925f2
Björn Roberg <bjorn.roberg@gmail.com>
2015-06-17 12:22:28 +0200

    added Either module in javascript added a bit to the readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # project-blinkenlights
 Here we have some odds and ends, a little bits and bobs, but also various knobs and lights.
+
+## Repo structure
+
+```python
+/ #generic repo files, such as this readme
+`-/code #contains code
+`-/tutorials
+```
---
+++ code/js/Either.es6.js
@@ -0,0 +1,35 @@
+/**
+* @module Either
+* @example
+let getThing = function () {
+  let text = Math.random() < 0.5 ?
+    'such success' : null;
+
+  if (text) {
+    return Either.right({information: text})
+  }
+  return Either.left(new Error('much error'))
+}
+
+let message = Either.match(getThing(), {
+  right: (result) => result.information,
+  left: (err) => err.message
+})
+*/
+let Either = function Either() {}
+let Left = function left(value) {
+  this.value = value
+}
+let Right = function right(value) {
+  this.value = value
+}
+Either.match = (either, matcher, dynamicThis) => {
+  return (matcher[either.constructor.name] || function () {})
+    .call(dynamicThis, (either || {}).value)
+}
+Either.left = (value) => {
+  return new Left(value)
+}
+Either.right = (value) => {
+  return new Right(value)
+}
