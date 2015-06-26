7cf7990b9222e9f1bc3fa9e8bb1e0d28f84925ba
Björn Roberg <bjorn.roberg@gmail.com>
2015-06-26 14:12:54 +0200

    more formatting

--- code/ps/profile/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ code/ps/profile/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -6,27 +6,27 @@
 
 function private:InitProfile()
 {
-	$profile_path = "$home\Documents\WindowsPowerShell"
+  $profile_path = "$home\Documents\WindowsPowerShell"
 
-	function im ($moduleName)
-	{
-		Import-Module "$profile_path\$moduleName.psm1" 3> $null
-	}
+  function im ($moduleName)
+  {
+    Import-Module "$profile_path\$moduleName.psm1" 3> $null
+  }
 
-	# Import our custom modules.
-	im "ui.base" # don't use with cmder
-	im "sp.init"
-	im "sp.tools"
+  # Import our custom modules.
+  im "ui.base" # don't use with cmder
+  im "sp.init"
+  im "sp.tools"
 
-	Import-Module "PowerTab"
+  Import-Module "PowerTab"
 
-	Write-Host " ___________ "
-	Write-Host "/  ___| ___ \"
-	Write-Host "\ '--.| |_/ /"
-	Write-Host " '--. \  __/ "
-	Write-Host "/\__/ / |    "
-	Write-Host "\____/\_|    "
-	Write-Host
+  Write-Host " ___________ "
+  Write-Host "/  ___| ___ \"
+  Write-Host "\ '--.| |_/ /"
+  Write-Host " '--. \  __/ "
+  Write-Host "/\__/ / |    "
+  Write-Host "\____/\_|    "
+  Write-Host
 }
 
 InitProfile
