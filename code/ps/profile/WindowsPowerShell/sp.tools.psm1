8ff8edd1ee2941fb9e3d91061c6a61c6db524621
Stina Qvarnström <stinaqv@gmail.com>
2015-07-08 17:32:38 +0200

    Function to see if feature is activated on a given scope

--- code/ps/profile/WindowsPowerShell/sp.tools.psm1
+++ code/ps/profile/WindowsPowerShell/sp.tools.psm1
@@ -135,3 +135,33 @@ function RestartNetwork ()
   net stop sptimerv4
   net start sptimerv4
 }
+
+function isFeatureEnabledOnScope
+(
+    [Parameter(Mandatory=$true)][string]$name,
+    [Parameter(Mandatory=$true)][string]$url,
+    [Parameter(Mandatory=$true)][string]$scope
+)
+{
+  if($scope -eq "Farm")
+  {
+    $params = @{
+      'Identity'            = $featureObject.Name;
+      'ErrorAction'         = 'SilentlyContinue';
+    }
+
+    $featureIsEnabled = (Get-SPFeature @params -Farm) -ne $null
+  }
+  else
+  {
+    $params = @{
+      'Identity'            = $featureObject.Name;
+      'ErrorAction'         = 'SilentlyContinue';
+      $featureObject.Scope  = $webAppUrl
+    }
+
+    $featureIsEnabled = (Get-SPFeature @params) -ne $null
+  }
+
+  return $featureIsEnabled
+}
